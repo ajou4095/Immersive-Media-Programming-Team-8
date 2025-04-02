c09a5fa504de52d713c3dea6da1b53111dba9222
Ray Jang <ajou4095@gmail.com>
2025-04-02 16:30:39 +0900

    Add DefaultEnemy

--- Assets/Scripts/enemy/Enemy.cs
+++ Assets/Scripts/enemy/Enemy.cs
@@ -1,33 +1,19 @@
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class Enemy : MonoBehaviour
+public abstract class Enemy : MonoBehaviour
 {
     public int maxHp = 0;
     public int hp = 0;
 
-    public GameObject attackIcon, healIcon;
-
     public Action action;
     public GameObject icon;
 
-    public Action NewAction()
+    public Enemy(int maxHp)
     {
-        if (hp * 2 < maxHp && Random.value < 0.5f)
-        {
-            action = new Heal(5, healIcon);
-        }
-        else
-        {
-            action = new Attack(5, attackIcon);
-        }
-
-        if (icon == null)
-        {
-            var position = transform.position + new Vector3(0, 1, 0);
-            Instantiate(action.getIcon(), position, transform.rotation);
-        }
-
-        return action;
+        this.maxHp = maxHp;
+        hp = this.maxHp;
     }
+
+    public abstract Action NewAction();
 }
